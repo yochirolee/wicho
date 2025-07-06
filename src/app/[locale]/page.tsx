1d1546c90138029fd113526c26f13bab440cf2d1
Yochiro lee <yleecruz@gmail.com>
2025-07-06 15:24:15 -0400

    multilanguage

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,25 +9,27 @@ import FAQSection from "../../components/FAQSection";
 import SecondaryCTA from "../../components/SecondaryCTA";
 import Footer from "../../components/Footer";
 
-type Props = {
+// ✅ Usa el tipo PageProps si estás usando Next.js 13+ con app directory
+type PageProps = {
   params: {
     locale: string
   }
 }
-export default async function Home({ params: { locale } }: Props) {
+
+export default async function Home({ params: { locale } }: PageProps) {
   const dict = await getDictionary(locale)
-//export default function Home() {
+
   return (
     <div className="flex flex-col min-h-screen">
       <HeroSection dict={dict} />
-      <OfferSection dict={dict}/>
-      <SocialProof dict={dict}/>
-      <FeaturesSection dict={dict}/>
-      <HowItWorks dict={dict}/>
-      <AboutSection dict={dict}/>   
-      <FAQSection dict={dict}/>
-      <SecondaryCTA dict={dict}/>
-      <Footer dict={dict}/>
+      <OfferSection dict={dict} />
+      <SocialProof dict={dict} />
+      <FeaturesSection dict={dict} />
+      <HowItWorks dict={dict} />
+      <AboutSection dict={dict} />
+      <FAQSection dict={dict} />
+      <SecondaryCTA dict={dict} />
+      <Footer dict={dict} />
     </div>
   );
 }
