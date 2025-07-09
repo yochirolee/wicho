c4178c75b2506798184bc91986e64a1919010b3b
Yochiro lee <yleecruz@gmail.com>
2025-07-09 11:08:18 -0400

    hero color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -108,7 +108,10 @@ const Footer: React.FC<{ dict: Dict }> = ({ dict }) => {
   const nav = dict.nav;
 
   return (
-    <footer className="bg-gradient-to-b from-green-700 via-green-800 to-green-900 text-white py-10">
+    <footer className="bg-gradient-to-br from-green-600 to-green-900 text-white py-10"
+      style={{
+        backgroundImage: 'linear-gradient(135deg, #16a34a 5%, #022c22 80%)',
+      }} >
       <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-10">
         {/* Logo y subtítulo */}
         <div>
